6943c91ee26675e70bc9ee8c6aae7feb6f0a0d06
krylorz <njoebgen@yahoo.com>
2013-12-10 03:35:04 -0600

    PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
+	public float speed = 6.0F;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +10,12 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKey(KeyCode.A)){
+			transform.Translate(Vector3.left * speed * Time.deltaTime);
+		}
+		
+		if(Input.GetKey(KeyCode.D)){
+			transform.Translate(Vector3.right * speed * Time.deltaTime);
+		}
 	}
 }
