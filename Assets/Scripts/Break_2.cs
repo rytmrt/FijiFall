fbd15776584b285b2a54a470168ccd020e14fa0b
YusukeMamiya <yuusuke.mamiya@gmail.com>
2013-12-11 09:51:16 +0900

    3type bumper make

---
+++ Assets/Scripts/Break_2.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Break_2 : MonoBehaviour {
+
+	private int counter = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (counter <= 0) {
+			Destroy (this.gameObject);
+		}
+	}
+
+	void OnCollisionEnter(Collision collisionInfo){
+		counter--;
+	}
+
+}
